76996f0cb8b3a40262a3e9e481cf5931f6194f94
ab <2553400824@qq.com>
2022-05-17 22:18:31 +0800

    绘制boss墙体

--- tank/tank-ts/src/canvas/wall.ts
+++ tank/tank-ts/src/canvas/wall.ts
@@ -11,6 +11,31 @@ export default new class extends canvasAbstract implements CanvasModel {
 
   render(): void {
     super.createModels()
+    this.createBossModels()
     super.renderModels()
+
+
+  }
+  createBossModels() {
+    const cw = config.canvas.width
+    const ch = config.canvas.height
+    const mw = config.model.width
+    const mh = config.model.height
+    const pos = [
+      { x: cw / 2 - mw * 2, y: ch - mh },
+      { x: cw / 2 - mw * 2, y: ch - mh * 2 },
+      { x: cw / 2 - mw * 2, y: ch - mh * 3 },
+      { x: cw / 2 - mw, y: ch - mh * 3 },
+      { x: cw / 2, y: ch - mh * 3 },
+      { x: cw / 2 + mw, y: ch - mh * 3 },
+      { x: cw / 2 + mw * 2, y: ch - mh * 3 },
+      { x: cw / 2 + mw * 2, y: ch - mh * 2 },
+      { x: cw / 2 + mw * 2, y: ch - mh },
+    ]
+    pos.forEach((position) => {
+      const model = this.model() as ModelConstructor
+      const instance = new model(position.x, position.y)
+      this.models.push(instance)
+    })
   }
 }('wall')
